c94b167b68d352a33613479b7c1ba676c7a1e661
J Mohit <jmohit1107@gmail.com>
2026-08-30 12:24:34 +0530

    [1] [Time Beats: 36.48%] [Memory Beats: 87.55%] - LeetPush

---
+++ 1-Two-Sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int arraySize = nums.size();
+        for(int i = 0; i < arraySize; i++){
+            for(int j = i + 1; j < arraySize; j++){
+                if(nums[i] + nums[j] == target){
+                    return {i, j};
+                }
+            }
+        }
+        return{};
+    }
+};
