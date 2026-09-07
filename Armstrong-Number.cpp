3bc842e41abdb7a00d8847d5ef0f59580194af04
Mohit <jmohit1107@gmail.com>
2026-09-07 21:00:50 +0530

    Refined Armstrong Number Solution

--- Armstrong-Number.cpp
+++ Armstrong-Number.cpp
@@ -3,12 +3,27 @@ class Solution {
     bool armstrongNumber(int n){
         
         int armstrong = 0;
+        int digits = 0;
         int number = n;
-        
+        int n1 = n;
+
+        while(n1 > 0){
+            n1 = n/10;
+            digits++;
+            
+        }
+
         while(n > 0){
-        
+
+            int power = 1;
             int ld = n % 10;
-            armstrong = armstrong + (ld * ld * ld);
+
+            for(int i = 0; i < digits; i++){
+                power = power * ld;
+            }
+
+
+            armstrong = armstrong + power;
             n = n/10;
             
         }
